0e74a12ee74147afd666f9e5af85b2e3ea4e8b85
Kamsi Ozo <86496597+floatlemagne@users.noreply.github.com>
2022-08-12 13:57:42 -0400

    Create rootfinding.py

---
+++ rootfinding.py
@@ -0,0 +1,45 @@
+# Method for computing the square root of a number a; uses Heron's method
+def sqrt(a):
+    if(a < 0):
+        return complex(0, sqrt(-a))
+    elif(a == 0):
+        return 0
+    
+    x = a
+    
+    while True:
+        x0 = x
+        x = (x + a/x)/2
+        if is_precise(x0, x, 1e-14):
+            break
+    return x
+  
+# Method for computing the nth root of a number a; uses Newton's method
+def root(n, a):
+    if (n == 0):
+        raise ValueError("Non-real error")
+    elif(n < 0 or type(n) != int):
+        raise ValueError("Invalid index")
+    elif(n % 2 == 0 and a < 0):
+        raise ValueError("Non-real error")
+    elif(n == 1):
+        return a
+
+    x = a
+
+    while True:
+        x0 = x
+        x = ((n-1)/n * x) + (a/n) * (1/pow(x, (n-1)))
+        
+        if is_precise(x0, x, 1e-14):
+            break
+
+    return x
+  
+# Method that determines whether the difference of two numbers is below a certain threshold value
+def is_precise(x1, x0, epsilon):
+    if epsilon <= 0:
+        raise ValueError("Non-positive error")
+    elif abs(x1-x0) < epsilon:
+        return True
+    return False
